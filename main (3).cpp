8a5f4fb8267568c78070ad65b0283d52f83abf6d
sadeemakhamis <135759761+sadeemakhamis@users.noreply.github.com>
2023-06-06 23:21:24 +0300

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,80 @@
+Kinetic Energy 
+A car with a mass of 700 kg is moving with a speed of 20m/s. Calculate the kinetic energy of the car? 
+Mass of the body m=700kg, 
+Velocity v=20m/s, 
+kinetic energy=140000 J
+
+
+
+
+Newton’s second law:F=ma 
+1- To find Force: 
+What net force is required to accelerate a car at a rate of 2 m/s2 if the car has a mass of 3,000 kg?
+m = 3000 kg
+a = 2 m/s^2
+F=6000N 
+
+
+
+2-To find mass: 
+What is the mass of a truck if it produces a force of 14,000 N while accelerating at a rate of 5 m/s2 ?
+a = 5 m/s^2 
+F=14,000N
+m = 2800 kg 
+
+
+3-To find acceleration: 
+What is the acceleration ofsoftball if it has a mass of 0.5 kg and hits the catcher's glove with a force of 25 N?
+m 0.5 kg
+F=25N 
+a = 50 m/s^2
+
+
+
+
+
+
+
+
+
+momentum law P=mv 
+calculate the momentum of a boat with a mass of 95 kg moving at 2 m/s?
+mass m =95kg
+velocity v = 2m/s 
+momentum = 190kgm/s
+
+
+Ideal gas law 
+Determine the volume of occupied by 4.54 grams of carbon dioxide gas at STP. 
+Solution: 
+2- To find Volume: 
+V = nRT / P 
+n=0.1 T=273 P=1 
+V = 2.240238 L
+
+
+1- To find Pressure: 
+4- 4.500 g of solid CO2 is put in an empty sealed 7.00 L container at a temperature of 273 K. When all the solid CO2 becomes gas, what will be the pressure in the container? 
+Solution: 
+P= nRT/V 
+n = 0.10225 mol T=273
+V=7 
+P = 0.327234765 atm
+3- To find mol: 
+2- 96.0 g. of a gas occupies 48.0 L at 700.0 mm Hg and 20.0 °C. What is its molecular weight? 
+Solution: 
+n = PV / RT 
+P=0.92 V= 48 T=293 
+n = [(700.0 mmHg / 760.0 mmHg atm??1) (48.0 L)] / [(0.08206 L atm mol??1 K??1) (293.0 K)] 
+n = 1.836664923 mol
+
+
+4- To find Temperature: 
+3- At what temperature will 0.789 moles of neon gas occupy 14.30 liters at 1.89 atmospheres? 
+Solution: 
+T = PV / nR 
+P=1.89 V=14.30 n=0.789 
+T = [(1.89 atm) (14.30 L)] / [(0.789 mol) (0.08206 L atm mol??1 K??1)] 
+T = 417.4354479 K 
+
+
